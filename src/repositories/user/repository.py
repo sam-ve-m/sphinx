4be430810e51ad621aee004dbd281363330d59c5
Anderson Faustino Lima <afl@lionx.com.br>
2021-05-26 09:29:58 -0300

    [TECH-35] implement tdd mock db

--- src/repositories/user/repository.py
+++ src/repositories/user/repository.py
@@ -1,7 +1,7 @@
 from src.repositories.base_repository import BaseRepository
-
+from decouple import config
 
 class UserRepository(BaseRepository):
 
     def __init__(self):
-        super.__init__(database='sphinx', collection='users')
+        super.__init__(database=config('MONGODB_DATABASE_NAME'), collection=config('MONGODB_USER_COLLECTION'))
---
+++ src/repositories/view/repository.py
@@ -0,0 +1,7 @@
+from src.repositories.base_repository import BaseRepository
+from decouple import config
+
+class ViewRepository(BaseRepository):
+
+    def __init__(self) -> None:
+        super().__init__(database=config('MONGODB_DATABASE_NAME'), collection=config('MONGODB_VIEW_COLLECTION'))
