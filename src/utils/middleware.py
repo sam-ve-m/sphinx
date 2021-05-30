6489258130534fc070ee42a97027106ec9356b1c
Marco Sievers <im.ximit@gmail.com>
2021-05-29 22:45:19 -0300

    [TECH-35][TECH-36] Controllers and services user + tdd (16)

--- src/utils/middleware.py
+++ src/utils/middleware.py
@@ -30,21 +30,22 @@ def need_be_admin(request: Request):
     )
 
 
-def user_is_allowed(user_data: dict, jwt_data: dict) -> bool:
+def user_not_allowed(user_data: dict, jwt_data: dict) -> bool:
     try:
         is_deleted = user_data.get("deleted")
         token_valid_after = user_data.get("token_valid_after")
-        token_created_at = datetime.parse(jwt_data.get("created_at"), )
-        is_token_valid = token_valid_after < token_created_at
-        return is_token_valid or is_deleted
-    except:
+        token_created_at = datetime.strptime(jwt_data.get("created_at"), '%Y-%m-%d %H:%M:%S.%f')
+        is_token_invalid = token_valid_after > token_created_at
+        return is_token_invalid or is_deleted
+    except Exception as e:
+        print(e)
         return False
 
 
 def validate_user(request: Request):
     user_repository = UserRepository()
     jwt_data = JWTHandler.get_payload_from_request(request=request)
-    if user_is_allowed(
+    if user_not_allowed(
             user_data=user_repository.find_one({"email": jwt_data["email"]}),
             jwt_data=jwt_data
     ):
