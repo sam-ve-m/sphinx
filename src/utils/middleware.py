372ee5970b6d69226232249f9c1388ed4df2a0a8
Anderson Faustino Lima <afl@lionx.com.br>
2021-06-21 17:05:29 -0300

    added thebes_ route to public paths

--- src/utils/middleware.py
+++ src/utils/middleware.py
@@ -20,7 +20,7 @@ def route_is_public(url_request: str) -> bool:
         raise NoPath("No path found")
 
     public_route = False
-    public_paths = ["/user", "/user/forgot_password", "/login", "/login/admin", "/term", "/docs", "/openapi.json"]
+    public_paths = ["/user", "/user/forgot_password", "/login", "/login/admin", "/term", "/docs", "/openapi.json", "/thebes_gate", "/thebes_answer"]
     if url_request in public_paths:
         public_route = True
     return public_route
