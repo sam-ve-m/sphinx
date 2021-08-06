50f0a780666fd8f8437c336fd8f8dfa534bc4d60
Marco Sievers de Almeida <msa@lionx.coim.br>
2021-08-06 16:12:29 -0300

    feat: finished

--- src/routers/validators/base.py
+++ src/routers/validators/base.py
@@ -74,7 +74,7 @@ class PIN(BaseModel):
 
 
 class ElectronicSignature(BaseModel):
-    electronic_signature: constr(regex=r'^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~])[a-zA-Z0-9!"#\$%&\'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]{8,10}$')
+    electronic_signature: constr(regex=r'^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{6,8}$')
 
 
 class Name(BaseModel):
