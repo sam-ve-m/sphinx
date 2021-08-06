46a02da23796e06c004b0bf0ada4cdc17360b574
Marco Sievers de Almeida <msa@lionx.coim.br>
2021-08-06 16:31:35 -0300

    feat: finished

--- src/services/builders/user/onboarding_steps_builder.py
+++ src/services/builders/user/onboarding_steps_builder.py
@@ -6,8 +6,8 @@ class OnboardingStepBuilder:
             "user_identifier_data_step": False,
             "user_selfie_step": False,
             "user_complementary_step": False,
-            "user_quiz_step": False,
             "user_electronic_signature": False,
+            "user_quiz_step": False,
             "finished": False,
         }
         self.__steps: list = [
@@ -52,8 +52,15 @@ class OnboardingStepBuilder:
 
         if marital is not None and (is_us_person is False or is_us_person is not None):
             self.__onboarding_steps["user_complementary_step"] = True
-            self.__onboarding_steps["current_onboarding_step"] = "user_quiz_step"
+            self.__onboarding_steps["current_onboarding_step"] = "electronic_signature"
 
+        return self
+
+    def user_user_electronic_signature(self, current_user):
+        has_electronic_signature = current_user.get("electronic_signature")
+        if has_electronic_signature is not None:
+            self.__onboarding_steps["user_electronic_signature"] = True
+            self.__onboarding_steps["current_onboarding_step"] = "user_quiz_step"
         return self
 
     def user_quiz_step(self, current_user):
@@ -61,15 +68,8 @@ class OnboardingStepBuilder:
 
         if has_stone_age_contract_uuid is not None:
             self.__onboarding_steps["user_quiz_step"] = True
-            self.__onboarding_steps["current_onboarding_step"] = "user_electronic_signature"
 
         return self
-
-    def user_user_electronic_signature(self, current_user):
-        has_electronic_signature = current_user.get("electronic_signature")
-
-        if has_electronic_signature is not None:
-            self.__onboarding_steps["user_electronic_signature"] = True
 
     def is_finished(self):
         if all([self.__onboarding_steps[step] for step in self.__steps]):
