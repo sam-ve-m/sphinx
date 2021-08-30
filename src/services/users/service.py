cfb672298a30bc1d1743c4b0e9e6e034239be367
Ilm <ilm@lionx.com.br>
2021-08-30 13:11:40 -0300

    feat: remove stone age mock from set_electronic_signature

--- src/services/users/service.py
+++ src/services/users/service.py
@@ -717,13 +717,6 @@ class UserService(IUser):
         if user_repository.update_one(old=old, new=new) is False:
             raise InternalServerError("common.process_issue")
 
-        # MOCK FEIO DA STONE AGE
-        payload = UserService.fake_stone_age_callback(
-            email=thebes_answer.get("email"), cpf=new.get("cpf")
-        )
-
-        SinacorService.process_callback(payload=payload)
-
         return {
             "status_code": status.HTTP_200_OK,
             "message_key": "requests.updated",
