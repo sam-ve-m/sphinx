9f7c4a3604e5fb83394ef2072fe3f656a0cdc520
Ilm <ilm@lionx.com.br>
2021-09-13 13:25:00 -0300

    fix: uncomment log.ini

--- main.py
+++ main.py
@@ -77,6 +77,6 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=8000,
         access_log=True,
-        # log_config="./log.ini",
+        log_config="./log.ini",
         log_level="info",
     )
